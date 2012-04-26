45c2dece752cc178b112d9d6407eaead8020b2c3
francisbyrne <byrnef87@gmail.com>
2012-04-26 21:54:07 +1000

    Added death face on loss

--- client/drawhangman.js
+++ client/drawhangman.js
@@ -4,11 +4,19 @@
 // if no guesses are passed in, show whole image
 var draw_hangman = function(guesses) {
 
-  // if guesses or attibrutes aren't set, get single canvas 
-  // and draw whole image
-  if (typeof guesses == 'undefined'
-      || typeof guesses.player_id == 'undefined'
-      || typeof guesses.left == 'undefined' ) {
+  var total_guesses = 10;
+  var colour = "#000000";
+  var line_width = 3;
+  var is_ingame = false;
+
+  // if guesses or attributes aren't set, get single canvas 
+  // and draw whole image (also sets whether dead face is drawn)
+  if (typeof guesses !== 'undefined'
+      && typeof guesses.player_id !== 'undefined'
+      && typeof guesses.left !== 'undefined' )
+    is_ingame = true;
+
+  if ( ! is_ingame) {
     var context = $("#hangCanvas")[0].getContext("2d");
     var guesses_left = 0
   } else {
@@ -18,16 +26,15 @@ var draw_hangman = function(guesses) {
                     .find("#hangCanvas")[0].getContext("2d");
     var guesses_left = guesses.left;
   }
-  
-  var i;
-  var total_guesses = 10;
-  var colour = "#000000";
+
   context.strokeStyle = colour;
+  context.lineWidth = line_width;
 
   // clear canvas before drawing anything
   clear_canvas(context);
 
   // countdown guesses left, drawing a part of image at each step
+  var i;
   for ( i = total_guesses; i >= guesses_left; i-- ) {
     switch (i) {
       case (9):
@@ -59,6 +66,10 @@ var draw_hangman = function(guesses) {
         break;
       case (0):
         draw_right_leg(context);
+
+        // if player loses, draw dead face
+        if (is_ingame) draw_death(context);
+
         break;
     }
   }
@@ -132,5 +143,29 @@ var draw_left_leg = function (context) {
 var draw_right_leg = function (context) {
   context.moveTo(130,130);
   context.lineTo(160,165);
+  context.stroke();
+};
+
+var draw_death = function (context) {
+  context.lineWidth = 2;
+
+  context.moveTo(119,52);
+  context.lineTo(127,60);
+  context.stroke();
+
+  context.moveTo(119,60);
+  context.lineTo(127,52);
+  context.stroke();
+
+  context.moveTo(141,52);
+  context.lineTo(133,60);
+  context.stroke();
+
+  context.moveTo(141,60);
+  context.lineTo(133,52);
+  context.stroke();
+
+  context.moveTo(120,70);
+  context.lineTo(140,70);
   context.stroke();
 };
